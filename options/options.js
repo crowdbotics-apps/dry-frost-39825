401b8bd7b265b119f5e13b2b1f8c485c1e6e3242
Crowdbotics <team@crowdbotics.com>
2023-04-11 03:01:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-frost-39825",
+  url: "https://dry-frost-39825.botics.co",
+  api: "https://dry-frost-39825.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
